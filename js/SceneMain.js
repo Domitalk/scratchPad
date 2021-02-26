a09b3a757f830701b13ce8835539c8470d2f063b
Dominic Chu <dominic.m.chu@gmail.com>
2021-02-25 22:21:31 -0500

    image props

--- js/SceneMain.js
+++ js/SceneMain.js
@@ -5,10 +5,13 @@ class SceneMain extends Phaser.Scene {
     }
     preload() {
     	// load img or sounds 
+        // the first arg is key, it'll be how we refer to this img from now 
+        this.load.image("face", "images/face.png");
     }
     create() {
         // define objs
-        console.log("Ready!");
+        this.face = this.add.image(100, 200, "key");    
+        // we can change the properties of this.face by calling functions on that this.face image class ie: this.face.displayHeight = 50
     }
     update() {
         // constantly running loop 
